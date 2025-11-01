a6c118026dd77ee3c4f1189767129a3ac19cce2a
Qeurlx <140286660+Qeurlx@users.noreply.github.com>
2025-11-01 15:29:08 -0500

    goon
    ggergfdgre

---
+++ goon.lua
@@ -0,0 +1,23 @@
+--[[ goon = "ts"..
+dksaiofdsa = "feewfewwfwe"
+
+while true ds.Loaded:Connect(function()
+    wait()
+    if goon == true .TouchEnded:Connect(function(hit)
+      else
+        break()
+    end)
+end)      ]]
+local goon = "ok"
+
+if goon == true then
+    while true do
+        local wait
+        print("goon")
+    end
+else
+    --break()
+    for index, value in ipairs(t) do
+        debug()
+    end
+end
