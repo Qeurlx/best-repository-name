f593a7cfa67a6d4445a27010ee29465129239f58
Redlavaa <98986258+Redlavaa@users.noreply.github.com>
2025-11-01 17:25:51 -0500

    Add linux.asm for Linux mode execution

---
+++ linux.asm
@@ -0,0 +1,29 @@
+if linux = mov abv 10
+
+// checks if linux
+
+
+
+
+// if deny true
+
+if not run windows mode
+
+
+
+else if
+
+
+
+
+ run linux mode
+
+
+
+
+def (linux mode)
+
+
+run python.linux.py
+
+// auto installled with goon package manager
