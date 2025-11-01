83b002ad931792dfbbfad3d4bbffd1ab70dc847e
Redlavaa <98986258+Redlavaa@users.noreply.github.com>
2025-11-01 16:26:22 -0500

    Revise goon.ts for improved flow and error handling
    Refactor goon.ts with new logic and structure

--- goon.ts
+++ goon.ts
@@ -1,7 +1,91 @@
-if ye = nah{
-  send error("how")
-
-else if ye != nah{
- print("0k")
+start goon(((((_))))))
+if gooner = false{
+ exit program becasue of ("not gooner")
 }
-|
+
+
+
+
+
+
+
+
+
+IF GOONER = GONNER {
+
+
+CONTINUYE WINDOIW OPEn
+
+
+
+
+
+
+
+
+  ts in ts[
+
+
+
+
+    end
+
+
+
+
+    ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  end game if {{{{
+
+
+
+
+    ts = ts}}}}}
+
+
+
+
+
+
+
+
+
+
+
+
+START PROGRAM AFTER RUN IF TRUE
+
+
+
+START()
+
+
+
+
+
+}
+
+
+start functioN(){
+
+  start(.pl)'s in folder
+
+}
+
+
+
+
